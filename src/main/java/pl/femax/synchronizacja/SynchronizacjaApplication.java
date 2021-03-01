00c4bd090ca4c1a9430db0ca489f5f62b45cbf3b
Konrad Krawczyk <k.krawczyk@femax.pl>
2021-03-01 16:20:23 +0100

    Created-schema-db

---
+++ src/main/java/pl/femax/synchronizacja/SynchronizacjaApplication.java
@@ -0,0 +1,13 @@
+package pl.femax.synchronizacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizacjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SynchronizacjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/femax/synchronizacja/SynchronizacjaApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.femax.synchronizacja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizacjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
